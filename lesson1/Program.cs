702e849f7b3b949b15749ab21fd80151123b4627
Anna Kireeva <aniutka.kireeva@mail.ru>
2023-03-26 22:41:21 +0700

    add lesson1

---
+++ lesson1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name);
+        }
+    }
+}
